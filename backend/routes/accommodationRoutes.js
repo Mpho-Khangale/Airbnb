aae27e6499e5ffd713d545adb6dbd6b981b13c86
mpho <khangalempho16@gmail.com>
2026-09-04 22:37:25 +0200

    fixing admin power

--- backend/routes/accommodationRoutes.js
+++ backend/routes/accommodationRoutes.js
@@ -7,23 +7,18 @@ const {
     deleteAccommodation
 } = require("../controllers/accommodationController");
 
-const { protect } = require("../middleware/auth");
+const { protect, adminOnly } = require("../middleware/auth");
 
 const router = express.Router();
 
-// Get all accommodations
 router.get("/", getAccommodations);
 
-// Get one accommodation
 router.get("/:id", getAccommodationById);
 
-// Create an accommodation
-router.post("/", protect, createAccommodation);
+router.post("/", protect, adminOnly, createAccommodation);
 
-// Update an accommodation
-router.put("/:id", protect, updateAccommodation);
+router.put("/:id", protect, adminOnly, updateAccommodation);
 
-// Delete an accommodation
-router.delete("/:id", protect, deleteAccommodation);
+router.delete("/:id", protect, adminOnly, deleteAccommodation);
 
 module.exports = router;
