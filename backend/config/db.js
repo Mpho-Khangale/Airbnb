5434601c11afb34014375d7532acf38206d2da68
mpho <khangalempho16@gmail.com>
2026-09-04 21:14:32 +0200

    fixing connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
             tls: true,
-            tlsAllowInvalidCertificates: false,
+            tlsAllowInvalidCertificates: true,
         });
         console.log("MongoDB connected successfully");
     } catch (error) {
