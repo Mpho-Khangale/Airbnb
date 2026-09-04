cc22246e427e969054089274bde7db53cc43dcbe
mpho <khangalempho16@gmail.com>
2026-09-04 20:00:40 +0200

    connect routes and import APIs

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
 
+const accommodationRoutes = require("./routes/accommodationRoutes");
+const userRoutes = require("./routes/userRoutes");
+
 dotenv.config();
 
 connectDB();
@@ -11,6 +14,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/accommodations", accommodationRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
     res.json({ message: "Airbnb API is running" });
