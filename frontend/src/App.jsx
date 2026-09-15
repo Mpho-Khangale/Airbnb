90f0c551eec4bb978e10d44a50b8c678f8cbc7c8
Mpho k <khangalempho16@gmail.com>
2026-09-15 19:30:00 +0200

    register and login imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Location from "./pages/Location";
 import ListingDetails from "./pages/ListingDetails";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
     return (
@@ -9,6 +11,8 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/location" element={<Location />} />
             <Route path="/listing/:id" element={<ListingDetails />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
         </Routes>
     );
 }
