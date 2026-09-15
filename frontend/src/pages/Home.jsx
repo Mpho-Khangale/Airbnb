823a6b5162df60f58a1bc1bf4cc6683f1d36976c
Mpho k <khangalempho16@gmail.com>
2026-09-15 18:01:04 +0200

    footer

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function Home() {
     return (
@@ -156,6 +157,7 @@ function Home() {
                     </div>
                 </section>
             </main>
+            <Footer />
         </>
     );
 }
