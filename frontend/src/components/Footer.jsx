0e1beec340d3db939898bca6e94b718d0c75b3df
Mpho k <khangalempho16@gmail.com>
2026-09-15 17:16:24 +0200

    footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,48 @@
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+
+                <div className="footer-column">
+                    <h3>Support</h3>
+                    <a href="#">Help Centre</a>
+                    <a href="#">AirCover</a>
+                    <a href="#">Anti-discrimination</a>
+                    <a href="#">Disability support</a>
+                    <a href="#">Cancellation options</a>
+                </div>
+
+                <div className="footer-column">
+                    <h3>Hosting</h3>
+                    <a href="#">Airbnb your home</a>
+                    <a href="#">AirCover for Hosts</a>
+                    <a href="#">Hosting resources</a>
+                    <a href="#">Community forum</a>
+                    <a href="#">Hosting responsibly</a>
+                </div>
+
+                <div className="footer-column">
+                    <h3>Airbnb</h3>
+                    <a href="#">Newsroom</a>
+                    <a href="#">New features</a>
+                    <a href="#">Careers</a>
+                    <a href="#">Investors</a>
+                    <a href="#">Gift cards</a>
+                </div>
+
+            </div>
+
+            <div className="footer-bottom">
+                <p>© 2026 Airbnb Clone. All rights reserved.</p>
+
+                <div className="footer-links">
+                    <a href="#">Privacy</a>
+                    <a href="#">Terms</a>
+                    <a href="#">Sitemap</a>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
