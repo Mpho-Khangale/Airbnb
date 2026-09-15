fae3d68f86bc3bd5dad205552710f7a34e319658
Mpho-Khangale <105310564+Mpho-Khangale@users.noreply.github.com>
2026-09-15 13:02:35 +0200

    imports and home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
     return (
-        <div>
-            <h1>Airbnb Clone</h1>
-            <p>Welcome to your Airbnb clone.</p>
-        </div>
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
     );
 }
 
