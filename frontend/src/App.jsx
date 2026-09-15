935f9e278fde863ca1061ca5c84445ac431f146d
Mpho k <khangalempho16@gmail.com>
2026-09-15 19:20:55 +0200

    listing import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Location from "./pages/Location";
+import ListingDetails from "./pages/ListingDetails";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/location" element={<Location />} />
+            <Route path="/listing/:id" element={<ListingDetails />} />
         </Routes>
     );
 }
